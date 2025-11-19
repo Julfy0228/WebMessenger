eec9ff9a7ddc5e92c94b2e8300f7662ee3c12113
Julfy0228 <bogdan43873official@gmail.com>
2025-11-19 14:31:07 +0300

    Добавьте файлы проекта.

---
+++ WebMessenger/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebMessenger.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime LastLogin { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
